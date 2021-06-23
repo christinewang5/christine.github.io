da248c2969f01e327290d9e4a7c84d28d0cad0ba
christinewang5 <chrissywang54@gmail.com>
2021-06-22 19:20:17 -0500

    initial

---
+++ gatsby-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  siteMetadata: {
+    title: "christine",
+  },
+  plugins: [
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-mdx",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
